47cfab28f0f9b396a88514ba5fc4ccdb743e771e
Thomasen007 <Thomas.elliott.nielson@gmail.com>
2023-01-21 23:42:37 -0800

    Change of a name

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const contactsController = require('../controller/contacts');
+const contactsController = require('../Controller/contacts');
 
 router.get('/', contactsController.getAll);
 
