04ed2f6b87982f09d7267fb60eb0e430d18f3278
Thomasen007 <Thomas.elliott.nielson@gmail.com>
2023-01-28 14:44:56 -0800

    one update to swagger.js

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
       title: 'Contacts API',
     description: 'This is a demonstration of MongoDB CRUD or Create, Read, Update, and Delete. The main features are listed below.',
   },
-  host:'api-doc-w04.onrender.com/',
+  host:'api-doc-w04.onrender.com',
   schemes: ['https']
 };
 
