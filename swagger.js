2afb3cb21657e7987de12e2bcc08b29a82a0cfcd
Thomasen007 <Thomas.elliott.nielson@gmail.com>
2023-01-28 15:44:38 -0800

    fix swagger

--- swagger.js
+++ swagger.js
@@ -3,8 +3,7 @@ const swaggerAutogen = require('swagger-autogen')();
 const doc = {
   info: {
     title: 'Contacts API',
-    description:
-      'This is a demonstration of MongoDB CRUD.'
+    description: 'This is a demonstration of MongoDB CRUD.'
   },
   host: 'api-doc-w04.onrender.com',
   schemes: ['https']
