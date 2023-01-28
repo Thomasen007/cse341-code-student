91542f5206e386a2385ab4ed82fc7aa59f878379
Thomasen007 <Thomas.elliott.nielson@gmail.com>
2023-01-28 14:55:52 -0800

    update swagger.js 1.2

--- swagger.js
+++ swagger.js
@@ -5,8 +5,18 @@ const doc = {
       title: 'Contacts API',
     description: 'This is a demonstration of MongoDB CRUD or Create, Read, Update, and Delete. The main features are listed below.',
   },
+  servers: [
+    {
+      url: "https://localhost:8080/",
+      description: "local server"
+    },
+    {
+      url: "https://api-doc-w04.onrender.com",
+      description: "deployed server"
+    }
+  ],
   host:'api-doc-w04.onrender.com',
-  schemes: ['https']
+  schemes: ['http']
 };
 
 const outputFile = './swagger.json';
